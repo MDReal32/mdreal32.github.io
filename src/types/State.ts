3bdd3d21412f4838eea076494d85fa67a7b2c163
MDReal32 <veysaliyev00@gmail.com>
2022-01-23 17:27:27 +0400

    pages

--- src/types/State.ts
+++ src/types/State.ts
@@ -1,11 +1,13 @@
-export type PageType = "main" | "about";
+import { DefineComponent } from "vue";
+import { PageType } from "../pages";
 
 export interface Page {
   name: string;
   type: PageType;
+  component: DefineComponent<any, any, any, any, any, any, any, any, any, any, any, any>;
 }
 
 export interface State {
-  page: number;
-  pages: Page[];
+  page: PageType;
+  pages: Record<PageType, Page>;
 }
