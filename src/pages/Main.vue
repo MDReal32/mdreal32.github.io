0a7cc87921ef94a6a69d24bea80ac6f15e4b8b60
MDReal32 <veysaliyev00@gmail.com>
2022-01-23 17:27:54 +0400

    Show on screen.

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,5 +1,26 @@
-<template>Home</template>
+<template>
+  <section v-for="field in fields">
+    <h2>{{ field }}</h2>
+    <Progressbar
+      v-for="{ percentage, text, progressbarColor } in progresses[field]"
+      progressbar-type="circle"
+      :progressbar-color="progressbarColor"
+      :text-color="progressbarColor"
+      :text="text"
+      :percentage="percentage"
+      :animation-duration="0.5"
+      :has-text-content="true"
+      size="80"
+      container-width="120"
+    />
+  </section>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Progressbar from "../components/Progressbar.vue";
+import { progresses } from "../config";
+
+const fields = Object.keys(progresses);
+</script>
 
 <style lang="scss" scoped></style>
