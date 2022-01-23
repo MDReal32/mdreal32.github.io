083d5fa8658cc3e142af6e664ad6a2f8a1ad52c3
MDReal32 <veysaliyev00@gmail.com>
2022-01-23 17:28:04 +0400

    About component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,11 +1,12 @@
 import { Page } from "../types/State";
 import Main from "./Main.vue";
+import About from "./About.vue";
 
 export type PageType = "main" | "about" | "frontend" | "backend";
 
 export const pages: Record<PageType, Page> = {
   main: { name: "Home", type: "main", component: Main },
-  about: { name: "About", type: "about", component: Main },
+  about: { name: "About", type: "about", component: About },
   frontend: { name: "Frontend Skills", type: "frontend", component: Main },
   backend: { name: "Backend Skills", type: "backend", component: Main }
 };
