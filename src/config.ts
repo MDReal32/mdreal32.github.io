4b58c61788649e48b4e416e0df1e856f593fa3d1
MDReal32 <veysaliyev00@gmail.com>
2022-01-23 17:23:35 +0400

    my knows on percents

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+import { Props } from "./types/Props";
+
+export const progresses: Record<string, Props[]> = {
+  General: [],
+  Frontend: [{ text: "JavaScript", percentage: 70, progressbarColor: "#E5CF1C" }],
+  Backend: []
+};
