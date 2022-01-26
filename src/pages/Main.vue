9ffe48eecc0b9ee01000517559e942632d237d90
MDReal32 <veysaliyev00@gmail.com>
2022-01-27 00:31:44 +0400

    Animation and data loading added

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,19 +1,34 @@
 <template>
-  <section v-for="field in fields">
-    <h2>{{ field }}</h2>
-    <Progressbar
-      v-for="{ percentage, text, progressbarColor } in progresses[field]"
-      progressbar-type="circle"
-      :progressbar-color="progressbarColor"
-      :text-color="progressbarColor"
-      :text="text"
-      :percentage="percentage"
-      :animation-duration="0.5"
-      :has-text-content="true"
-      :size="80"
-      :container-width="120"
-    />
-  </section>
+  <div :class="{ 'container': true, 'content-is-loading': isOverflow }">
+    <transition name="fade">
+      <section class="loading" v-if="!isLoadedData">
+        <div class="progress-container">
+          <h2 class="loading-data">Please wait. Loading data about me{{ getDots }}</h2>
+          <Progressbar :percentage="loadedPercent" :is-percent-shown="true" />
+        </div>
+      </section>
+    </transition>
+
+    <transition name="fade" v-for="field in fields">
+      <section class="section" v-if="isLoadedData">
+        <h2>{{ field }}</h2>
+        <div class="bars">
+          <Progressbar
+            v-for="{ percentage, text, color } in progresses[field]"
+            progressbar-type="circle"
+            :progressbar-color="color"
+            :text-color="color"
+            :text="text"
+            :percentage="percentage"
+            :animation-duration="0.5"
+            :has-text-content="true"
+            :size="80"
+            :await="0.8"
+          />
+        </div>
+      </section>
+    </transition>
+  </div>
 </template>
 
 <script setup lang="ts">
