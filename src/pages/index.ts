1fdb00dc36cce8ce10952e5a3550629e8476a928
MDReal32 <veysaliyev00@gmail.com>
2022-01-27 00:31:24 +0400

    New Pages Added

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,12 +1,14 @@
 import { Page } from "../types/State";
 import Main from "./Main.vue";
 import About from "./About.vue";
+import Frontend from "./Frontend.vue";
+import Backend from "./Backend.vue";
 
 export type PageType = "main" | "about" | "frontend" | "backend";
 
 export const pages: Record<PageType, Page> = {
   main: { name: "Home", type: "main", component: Main },
   about: { name: "About", type: "about", component: About },
-  frontend: { name: "Frontend Skills", type: "frontend", component: Main },
-  backend: { name: "Backend Skills", type: "backend", component: Main }
+  frontend: { name: "Frontend Skills", type: "frontend", component: Frontend },
+  backend: { name: "Backend Skills", type: "backend", component: Backend }
 };
