eee5da93313d4c2acebb3431b47f78eaa52858ee
MDReal32 <veysaliyev00@gmail.com>
2022-01-27 00:33:22 +0400

    Dummy data removed

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-import { Props } from "./types/Props";
-
-export const progresses: Record<string, Props[]> = {
-  General: [],
-  Frontend: [{ text: "JavaScript", percentage: 70, progressbarColor: "#E5CF1C" }],
-  Backend: []
-};
