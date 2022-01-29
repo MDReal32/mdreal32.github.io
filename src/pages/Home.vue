cf94eb5867b6a3468e8666d57f585fa382bd7df9
MDReal32 <veysaliyev00@gmail.com>
2022-01-29 21:28:59 +0400

    refactor

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,29 +1,29 @@
 <template>
   <div :class="{ 'container': true, 'content-is-loading': isOverflow }">
     <transition name="fade">
-      <section class="loading" v-if="!isLoadedData">
+      <section v-if="!isLoadedData" class="loading">
         <div class="progress-container">
           <h2 class="loading-data">Please wait. Loading data about me{{ getDots }}</h2>
-          <Progressbar :percentage="loadedPercent" :is-percent-shown="true" />
+          <Progressbar :is-percent-shown="true" :percentage="loadedPercent" />
         </div>
       </section>
     </transition>
 
-    <transition name="fade" v-for="field in fields">
+    <transition v-for="field in fields" name="fade">
       <section class="section">
         <h2>{{ field }}</h2>
         <div class="bars">
           <Progressbar
             v-for="{ percentage, text, color } in progresses[field]"
-            progressbar-type="circle"
-            :progressbar-color="color"
-            :text-color="color"
-            :text="text"
-            :percentage="percentage"
             :animation-duration="0.5"
-            :has-text-content="true"
-            :size="80"
             :await="0.8"
+            :has-text-content="true"
+            :percentage="percentage"
+            :progressbar-color="color"
+            :size="80"
+            :text="text"
+            :text-color="color"
+            progressbar-type="circle"
           />
         </div>
       </section>
