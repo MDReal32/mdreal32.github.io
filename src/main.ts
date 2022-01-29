e56dc5b0c459bbedc15b0882323058591024a3e3
MDReal32 <veysaliyev00@gmail.com>
2022-01-29 21:19:38 +0400

    imports

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import { createApp as _createApp, createSSRApp } from "vue";
 import App from "./App.vue";
-import { createApp } from "vue";
-import { store } from "./store/store";
+import { store } from "./store";
+import Axios from "axios";
+import { router } from "./router";
+import { isSSR } from "./config";
+import { Progress } from "./types/Progress";
 
 export const app = createApp(App).use(store);
