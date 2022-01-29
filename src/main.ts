8e154ce8e7802b3275c0f304fc2335d61b321eec
MDReal32 <veysaliyev00@gmail.com>
2022-01-29 21:19:52 +0400

    export app changed to export createApp function

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,39 @@ import { router } from "./router";
 import { isSSR } from "./config";
 import { Progress } from "./types/Progress";
 
-export const app = createApp(App).use(store);
+export const createApp = async () => {
+  const config = await Axios.get<Progress>("https://raw.githubusercontent.com/MDReal32/MDReal32/master/config.json");
+  await store.dispatch("setConfig", config.data);
+  await store.dispatch("setConfig", {
+    General: [
+      { text: "JavaScript", percentage: 60, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 30, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 25, color: "#E5CF1C" }
+    ],
+    Frontend: [
+      { text: "JavaScript", percentage: 60, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 20, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 25, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 20, color: "#E5CF1C" }
+    ],
+    Backend: [
+      { text: "JavaScript", percentage: 60, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 30, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 20, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 25, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 30, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 20, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
+      { text: "JavaScript", percentage: 20, color: "#E5CF1C" }
+    ]
+  });
+
+  return (isSSR ? createSSRApp : _createApp)(App).use(router).use(store);
+};
