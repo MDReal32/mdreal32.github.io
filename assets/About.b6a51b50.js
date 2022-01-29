58710ef28cd6dd5a414b96b3f1ab480bf728302c
MDReal32 <40066034+MDReal32@users.noreply.github.com>
2022-01-29 17:44:43 +0000

    Build: (ed1d89a) IDE Configuration

---
+++ assets/About.b6a51b50.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.77dd4307.js";import"./vendor.c56b455f.js";const e={};function t(o,c){return"About"}var s=r(e,[["render",t]]);export{s as default};
---
+++ assets/Frontend.49988e76.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.77dd4307.js";import"./vendor.c56b455f.js";const e={};function n(t,o){return"Frontend"}var _=r(e,[["render",n]]);export{_ as default};
