8b4666f82f80db30331cd3bd0569a25eed40a219
MDReal32 <veysaliyev00@gmail.com>
2022-01-29 21:17:06 +0400

    pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,14 +1,3 @@
-import { Page } from "../types/State";
-import Main from "./Main.vue";
-import About from "./About.vue";
-import Frontend from "./Frontend.vue";
-import Backend from "./Backend.vue";
+export const order = ["home", "about", "frontend", "backend", "cv"];
 
-export type PageType = "main" | "about" | "frontend" | "backend";
-
-export const pages: Record<PageType, Page> = {
-  main: { name: "Home", type: "main", component: Main },
-  about: { name: "About", type: "about", component: About },
-  frontend: { name: "Frontend Skills", type: "frontend", component: Frontend },
-  backend: { name: "Backend Skills", type: "backend", component: Backend }
-};
+export const index = "home";
