dbd8d6f97df087db4367d11924d065f059159299
MDReal32 <veysaliyev00@gmail.com>
2022-01-30 10:12:39 +0400

    Only Client render preloader component

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -1,5 +1,10 @@
+import { createApp as createPreloaderApp } from "vue";
 import { createApp } from "./main";
 import { router } from "./router";
+import Preloader from "./components/Preloader.vue";
+import { store } from "./store";
+
+createPreloaderApp(Preloader).use(store).mount(".preloader .preloader-container");
 
 (async () => {
   const app = await createApp();
