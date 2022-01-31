e1712ada201889372b0150a489ff3c534cc9dbdc
MDReal32 <veysaliyev00@gmail.com>
2022-01-31 11:31:28 +0400

    get from main

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -1,10 +1,7 @@
-import { createApp as createPreloaderApp } from "vue";
-import { createApp } from "./main";
+import { createApp, preloaderApp } from "./main";
 import { router } from "./router";
-import Preloader from "./components/Preloader.vue";
-import { store } from "./store";
 
-createPreloaderApp(Preloader).use(store).mount(".preloader .preloader-container");
+preloaderApp.mount(".preloader .preloader-container");
 
 (async () => {
   const app = await createApp();
