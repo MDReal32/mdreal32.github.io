310e3df456fa25e2819e1a52a6aec0d0c7c18464
MDReal32 <veysaliyev00@gmail.com>
2022-02-02 20:36:17 +0400

    Parse from config

--- src/main.ts
+++ src/main.ts
@@ -4,44 +4,16 @@ import { store } from "./store";
 import Axios from "axios";
 import { router } from "./router";
 import { isSSR } from "./config";
-import { Config } from "./types/Config";
+import { Data } from "./types/Data";
 import Preloader from "./components/Preloader.vue";
 
 export const preloaderApp = _createApp(Preloader).use(store);
 
 export const createApp = async () => {
-  const config = await Axios.get<Config>("https://raw.githubusercontent.com/MDReal32/MDReal32/master/config.json");
-  await store.dispatch("setConfig", config.data);
-  await store.dispatch("setConfig", {
-    General: [
-      { text: "JavaScript", percentage: 60, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 30, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 25, color: "#E5CF1C" }
-    ],
-    Frontend: [
-      { text: "JavaScript", percentage: 60, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 20, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 25, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 20, color: "#E5CF1C" }
-    ],
-    Backend: [
-      { text: "JavaScript", percentage: 60, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 30, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 20, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 25, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 30, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 20, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 70, color: "#E5CF1C" },
-      { text: "JavaScript", percentage: 20, color: "#E5CF1C" }
-    ]
-  });
+  const { data: config } = await Axios.get<Data>(
+    "https://raw.githubusercontent.com/MDReal32/MDReal32/master/config.json"
+  );
+  await store.dispatch("setConfig", config);
 
   return (isSSR ? createSSRApp : _createApp)(App).use(router).use(store);
 };
