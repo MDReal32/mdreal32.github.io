84f1e1959622bd3ba151ee107b2b441d70cc2c74
MDReal32 <40066034+MDReal32@users.noreply.github.com>
2022-02-07 06:42:55 +0000

    Build: (e03579a) simplified

--- assets/Backend.ddfb3202.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.522c0a3c.js";import"./vendor.3d62933d.js";const r={};function c(n,a){return"Backend"}var _=e(r,[["render",c]]);export{_ as default};
--- assets/Frontend.1b304959.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.522c0a3c.js";import"./vendor.3d62933d.js";const e={};function n(t,c){return"Frontend"}var _=r(e,[["render",n]]);export{_ as default};
