c0c9729ba2a499aa92e6b70c18930fdd1ab4ef97
MDReal32 <veysaliyev00@gmail.com>
2022-02-16 17:46:37 +0400

    modified

--- prerender.ts
+++ prerender.ts
@@ -1,32 +1,33 @@
-import { readFileSync, writeFileSync } from "fs";
-import { resolve as res, sep } from "path";
+import { mkdirSync, readFileSync, writeFileSync } from "fs";
+import { resolve as res, dirname } from "path";
 import { ServerModule } from "./src/types/ServerRenderFunction";
-import { glob } from "glob";
 import { getHtml } from "./server/utils/getHtml";
+import { getPageRoutes } from "./src/utils/pageRoutes";
+import Axios from "axios";
+import type { Data } from "./src/types/Data";
+import { store } from "./src/store";
 
 const resolve = (...p: string[]) => res(__dirname, ...p);
 const template = readFileSync("build/index.html", "utf-8");
 const { render } = require("./build/server-bundle/entry-server") as ServerModule;
 
 const root = resolve("src/pages");
-glob(res(root, "**", "*.vue"), { cwd: root }, async (err, matches) => {
-  if (err) throw err;
 
-  const files = matches.map(match =>
-    res(match)
-      .replace(`${root}${sep}`, "")
-      .replace(/\.vue$/, "")
+(async () => {
+  const { data: config } = await Axios.get<Data>(
+    "https://raw.githubusercontent.com/MDReal32/MDReal32/master/config.json"
   );
-
-  for (const file of files) {
-    const endpoint = `/${file.toLowerCase()}`;
+  const pageRoutes = getPageRoutes(config);
+  await store.dispatch("setConfig", config);
+  for (const route of pageRoutes) {
+    const endpoint = route.path.toLowerCase();
     const destinationFile = resolve(`build/${endpoint}.html`);
-
     const html = await getHtml({ url: endpoint, render, isProd: true, template });
+    mkdirSync(dirname(destinationFile), { recursive: true });
     writeFileSync(destinationFile, html);
   }
 
   const destinationFile = resolve(`build/index.html`);
   const html = await getHtml({ url: "/", render, isProd: true, template });
   writeFileSync(destinationFile, html);
-});
+})();
