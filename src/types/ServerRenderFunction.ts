1d54e595c32c240bef68e54e48607a25c1ea5a60
MDReal32 <veysaliyev00@gmail.com>
2022-03-04 13:09:53 +0400

    config removed

--- src/types/ServerRenderFunction.ts
+++ src/types/ServerRenderFunction.ts
@@ -1,12 +1,11 @@
 import { SSRContext } from "vue/server-renderer";
-import { Data } from "./Data";
 
 export interface ServerRenderFunctionResponse {
   html: string;
   context: SSRContext;
 }
 
-export type ServerRenderFunction = (url: string, config: Data) => Promise<ServerRenderFunctionResponse>;
+export type ServerRenderFunction = (url: string) => Promise<ServerRenderFunctionResponse>;
 
 export interface ServerModule {
   render: ServerRenderFunction;
